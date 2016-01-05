52218d2c6321b1cfc91273868a9c0b40d671af0f
Artur Czepiel <czepiel.artur@gmail.com>
2016-01-05 12:20:07 +0100

    First draft of count_estimate

---
+++ psql_estimate/__init__.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+from django.db import connection
+from django.contrib.humanize.templatetags.humanize import intcomma, intword
+
+
+def humanize(value):
+    try:
+        number, word = intword(value).split()
+    except AttributeError:
+        # lower than 1M
+        return intcomma(value, use_l10n=False)
+    word = word[0].upper()
+    return "%s %s" % (number, word)
+
+
+def count_estimate(query, short=True):
+    cursor = connection.cursor()
+    cursor.execute("SELECT count_estimate('%s')" % query)
+    count = cursor.fetchone()[0]
+
+    if short:
+        return humanize(count)
+    return count
